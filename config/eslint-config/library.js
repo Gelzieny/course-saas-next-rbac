00bf47abfc71f80025ccda98a6a303024581bd08
Gelzieny R. Martins <gmail.com>
2025-04-18 11:30:09 -0300

    Prettier & ESLint no Monorepo

--- config/eslint-config/library.js
+++ config/eslint-config/library.js
@@ -1,10 +1,10 @@
 /**
- * A custom ESLint configuration for libraries that use Next.js.
+ * A shared ESLint configuration for the repository.
  *
  * @type {import("eslint").Linter.Config[]}
  * */
 module.exports = {
-  extends: ['@rocketseat/eslint-config/next'],
+  extends: ['@rocketseat/eslint-config/react'],
   plugins: ['simple-import-sort'],
   rules: {
     'simple-import-sort/imports': 'error',
