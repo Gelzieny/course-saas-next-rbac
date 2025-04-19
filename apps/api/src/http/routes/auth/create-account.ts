d100a36e659ee7af68a38dd2645220aeef9ecf66
Gelzieny R. Martins <gmail.com>
2025-04-19 14:05:05 -0300

    Erro ao gerar o arquivo Swagger

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,16 +1,17 @@
-import { z } from 'zod'
 import { hash } from 'bcryptjs'
 import type { FastifyInstance } from 'fastify'
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
 
 import { prisma } from '@/lib/prisma'
-
 
 export async function createAccount(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
     '/users',
     {
       schema: {
+        tags: ['Auth'],
+        summary: 'Create a new account',
         body: z.object({
           name: z.string(),
           email: z.string().email(),
